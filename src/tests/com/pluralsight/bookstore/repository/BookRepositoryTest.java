e83ae8a8f579dde348e6ebeadb573838b391838c
BSZA <BSZA>
2017-08-22 16:56:03 +0100

    Initial commit

---
+++ src/tests/com/pluralsight/bookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.pluralsight.bookstore.repository;
+
+import com.pluralsight.bookstore.model.Book;
+import com.pluralsight.bookstore.model.Language;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.core.api.annotation.Inject;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class BookRepositoryTest {
+    @Inject
+    private BookRepository bookRepository;
+    @Test
+    public void create() throws Exception {
+        //Test counting book
+        bookRepository.countAll();
+        //assertEquals(Long.valueOf(0),bookRepository.countAll());
+        //assertEquals(0,bookRepository.findAll().size());
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(BookRepository.class)
+                .addClass(Book.class)
+                .addClass(Language.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource("META-INF/test-persistence.xml","persistence.xml");
+    }
+
+}
